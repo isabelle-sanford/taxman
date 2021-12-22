ae894efcccc18e150ed8cf4185f36676fdb4b706
Isabelle Sanford <isabellesanford@gmail.com>
2021-12-22 14:27:18 -0500

    more cleaning, work on webpage responsive table

--- webpage/index.js
+++ webpage/index.js
@@ -27,11 +27,53 @@ function runEnter() {
   let table = d3.select("tbody");
   table.html("");
 
-  for (let i = 0; i <= inputValue / 10; i++) {
+  let numFullRows = Math.round(inputValue / 10 - 1); // not sure the round is needed
+  for (let i = 0; i < numFullRows; i++) {
     let row = table.append("tr");
     for (let j = 0; j <= 10; j++) {
       let cell = row.append("td");
-      cell.text(i * 10 + j);
+      let num = i * 10 + j + 1
+      cell.text(num); // starting at 1!
+      cell.attr('class', 'available');
+      cell.attr('id', 'n' + num); //ew
+      cell.on('click', playerPick);
     }
   }
+  let lastRow = table.append("tr");
+  for (let k = 0; k < inputValue % 10; k++) {
+    let cell = lastRow.append("td")
+    cell.text(numFullRows * 10 + k + 1);
+    cell.attr('class', 'available')
+  }
 }
+
+let table = d3.select("tbody")
+let tableEvent = table.on("mouseover", highlighting)
+let tableClick = table.on("click", playerPick)
+
+function playerPick() {
+  console.log("picked a thing!")
+  d3.selectAll(".available").on("click", function () {
+    d3.select(this).style("background-color", "green").attr("class", "unavailable")
+  })
+  //d3.select("#n" + num).style('background', 'black');
+}
+
+function highlighting() {
+  d3.selectAll(".available")
+    .on("mouseover", function () {
+      d3.select(this)
+        .style("background-color", "orange");
+
+      // Get current event info
+      console.log(d3.event);
+
+      // Get x & y co-ordinates
+      console.log(d3.mouse(this));
+    })
+    .on("mouseout", function () {
+      d3.select(this)
+        .style("background-color", "white")
+    });
+}
+
